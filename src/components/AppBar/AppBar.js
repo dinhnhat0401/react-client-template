bb1840c7bb1feb2e205c713401ba8d21f6b0ced7
dinhnhat0401 <nhatphutho@gmail.com>
2020-06-30 23:29:34 +0900

    wip

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import Landing from "../Landing/index";
+import NotFoundPage from "../NotFoundPage/index";
 
 function AppBar() {
   return (
-    <div>
-      <p>AppBar here</p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/landing" component={Landing} />
+        <Route path="/abc" component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <article>
+      <h1>
+        <p>This is not found page</p>
+      </h1>
+    </article>
+  );
+}
